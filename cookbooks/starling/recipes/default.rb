925b7710cfcbc58c07ebecb0d0bc0c8e6efdfc81
kiosan <obondar@gmail.com>
2010-02-19 16:21:12 +0200

    fix

--- cookbooks/starling/recipes/default.rb
+++ cookbooks/starling/recipes/default.rb
@@ -3,10 +3,15 @@ require 'pp'
 # Cookbook Name:: starling
 # Recipe:: default
 #
-
+first = true
+app_name1 = ""
+node[:applications].each do |app_name,data|
+  app_name1 = app_name if first
+  first = false  
+end
 if ['solo', 'app', 'app_master'].include?(node[:instance_role])
    execute "start-starling" do
-     command "cd /data/#{app_name}/current"
+     command "cd /data/#{app_name1}/current"
      command "RAILS_ENV=production ./script/workling_client start"
      not_if "pgrep workling"
    end
