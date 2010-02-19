61907d68bc7bcebc82f467f12524a3b042e71f48
kiosan <obondar@gmail.com>
2010-02-19 16:50:35 +0200

    fix

--- cookbooks/starling/recipes/default.rb
+++ cookbooks/starling/recipes/default.rb
@@ -10,9 +10,12 @@ node[:applications].each do |app_name,data|
   first = false  
 end
 if ['solo', 'app', 'app_master'].include?(node[:instance_role])
+   execute "make_it_exec" do
+     command "chmod +x /data/#{app_name1}/current/script/workling_client"
+   end
    execute "start-starling" do
      command "cd /data/#{app_name1}/current"
-     command "RAILS_ENV=production ./script/workling_client start"
+     command "RAILS_ENV=production /data/#{app_name1}/current/script/workling_client start"
      not_if "pgrep workling"
    end
 end
